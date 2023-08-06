e11f939f707095e5674397fcef39b08678e55932
Ildar <ildarvagapov528@gmail.com>
2023-08-06 22:22:55 +0500

    g

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setSort } from "../redux/filter/slice"
 import { selectSort } from "../redux/filter/selectors"
@@ -14,14 +14,27 @@ export const Sort = () => {
 
 	const dispatch = useDispatch()
 	const sort = useSelector(selectSort)
+	const sortRef = useRef();
 
 	const addSort = (obj) => {
 		dispatch(setSort(obj))
 		setOpen(false)
 	}
 
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (sortRef.current && !event.composedPath().includes(sortRef.current)) {
+				setOpen(false);
+			}
+		};
+
+		document.body.addEventListener('click', handleClickOutside);
+
+		return () => document.body.removeEventListener('click', handleClickOutside);
+	}, []);
+
 	return (
-		<div className="sort">
+		<div ref={sortRef} className="sort">
 			<div className="sort__label">
 				<svg
 					width="10"
